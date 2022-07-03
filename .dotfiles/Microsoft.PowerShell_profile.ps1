8efcba40a20ac7113d4f834995af15b5f4605ed5
s0ph1stry <antojimdev@gmail.com>
2022-07-03 18:13:09 +0200

    fix readme

---
+++ .dotfiles/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+## (User only) Path for this config: $HOME\Documents\WindowsPowerShell
+## (All users) Path for this config: C:\Windows\System32\WindowsPowerShell\v1.0
+## run '$profile' in ps for profile path
+
+## Adding functions to ps: New-Alias <shortname> <function_name>
+
+function goToProjects {
+    Set-Location F:\MyProjects
+}
