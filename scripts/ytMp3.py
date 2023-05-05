86ed2e8fe0d47cecac067af751cd16afe30939ff
Antonio <antojimdev@gmail.com>
2023-05-05 11:50:11 +0200

    refactor, pytube issue 'streamingData' https://github.com/pytube/pytube/issues

--- scripts/ytMp3.py
+++ scripts/ytMp3.py
@@ -2,32 +2,36 @@ from pytube import YouTube
 import os, platform, shutil
 
 ### VARIABLES
-MUSIC_PATH_WINDOWS             = os.path.expanduser('~\Music')
-MUSIC_PATH_LINUX             = os.path.expanduser('~/Music')
+MUSIC_PATH_WINDOWS  = os.path.expanduser('~\Music')
+MUSIC_PATH_LINUX    = os.path.expanduser('~/Music')
+YT_VIDEO            = None
 
 ### FUNCTIONS
-def video_download(url):
+def mp3_format_video_download(url):
     """ 
     Download the video and move it to music folder 
     Arguments:
         url:        youtube video url
         sys_os:     OS running script
     """
-    yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
-    downloaded_file = video.download()
-    base, ext = os.path.splitext(downloaded_file)
-    new_file = base + '.mp3'
-    os.rename(downloaded_file, new_file)
+    YT_VIDEO = YouTube(url)
+    # video = YT_VIDEO.streams.filter(only_audio=True).first()
+    video = YT_VIDEO.title
+    # downloaded_file = video.download()
+    # base= os.path.splitext(downloaded_file)
+    # new_file = base + '.mp3'
+    # os.rename(downloaded_file, new_file)
     # print(f"{new_file}")
-    if(platform.system() == 'Linux'):
-        splitted_path = new_file.split('/')
-        # /{splitted_path.pop()}
-        shutil.move(f"{new_file}", f"{MUSIC_PATH_LINUX}")
-    else:
-        shutil.move(f"{new_file}", f"{MUSIC_PATH_WINDOWS}")
-        
+    print(f"{video}")
     
+    
+def move_to_music_folder(file):
+    if(platform.system() == 'Linux'):
+        splitted_path = file.split('/')
+        # /{splitted_path.pop()}
+        shutil.move(f"{file}", f"{MUSIC_PATH_LINUX}")
+    else:
+        shutil.move(f"{file}", f"{MUSIC_PATH_WINDOWS}")
 
 
 ############# MAIN #############
@@ -37,7 +41,7 @@ while True:
     url = str(input('url: '))
     if url == "exit":
         break;
-    video_download(url)
+    mp3_format_video_download(url)
 
 print("Done")
 
